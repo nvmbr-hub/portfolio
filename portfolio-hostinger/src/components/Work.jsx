838b836ef49cb0f291207259e164973a06a68def
nmbr-hub <v.simutin@brl.ru>
2023-08-27 16:11:16 +0700

    Create project Vite + React + Tailwindcss for my portfolio

---
+++ portfolio-hostinger/src/components/Work.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import WorkItem from './WorkItem'
+
+const data = [
+    {
+        year: 2020,
+        title: 'Wordpress developer',
+        duration: '5 years',
+        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nihil molestias iure dolorem, sequi repudiandae minima culpa, ea veniam, ducimus excepturi tempora architecto velit ad nulla illo ipsam qui ipsa hic cum fugit alias aut sint ut? Incidunt eius qui rerum odio. Qui earum vero fuga eum veritatis libero corrupti.'
+    },
+    {
+        year: 2020,
+        title: 'Wordpress developer',
+        duration: '5 years',
+        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nihil molestias iure dolorem, sequi repudiandae minima culpa, ea veniam, ducimus excepturi tempora architecto velit ad nulla illo ipsam qui ipsa hic cum fugit alias aut sint ut? Incidunt eius qui rerum odio. Qui earum vero fuga eum veritatis libero corrupti.'
+    },
+    {
+        year: 2020,
+        title: 'Wordpress developer',
+        duration: '5 years',
+        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nihil molestias iure dolorem, sequi repudiandae minima culpa, ea veniam, ducimus excepturi tempora architecto velit ad nulla illo ipsam qui ipsa hic cum fugit alias aut sint ut? Incidunt eius qui rerum odio. Qui earum vero fuga eum veritatis libero corrupti.'
+    },
+    {
+        year: 2020,
+        title: 'Wordpress developer',
+        duration: '5 years',
+        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nihil molestias iure dolorem, sequi repudiandae minima culpa, ea veniam, ducimus excepturi tempora architecto velit ad nulla illo ipsam qui ipsa hic cum fugit alias aut sint ut? Incidunt eius qui rerum odio. Qui earum vero fuga eum veritatis libero corrupti.'
+    },
+]
+
+const Work = () => {
+  return (
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+
+    </div>
+  )
+}
+
+export default Work
